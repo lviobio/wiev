dcc054ecff3941a8745667ea2d0b709a486d900b
Daniel Ogirev <marsik025@gmail.com>
2025-09-14 21:17:36 +0300

    feat: install unplugin vue components & auto import

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,75 @@
-import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig, loadEnv } from 'vite';
-import path from "path";
+import laravel from 'laravel-vite-plugin'
+import path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig, loadEnv } from 'vite'
 
-export default defineConfig(({mode}) => {
-    const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
 
-    return {
-        server: {
-            port: env.EXPOSE_VITE_PORT,
-            hmr: {
-                host: 'localhost',
-            },
+  return {
+    server: {
+      port: env.EXPOSE_VITE_PORT,
+      hmr: {
+        host: 'localhost',
+      },
+    },
+    plugins: [
+      laravel({
+        input: ['resources/js/app.ts'],
+        ssr: 'resources/js/ssr.ts',
+        refresh: true,
+      }),
+      tailwindcss(),
+      vue({
+        template: {
+          transformAssetUrls: {
+            base: null,
+            includeAbsolute: false,
+          },
         },
-        plugins: [
-            laravel({
-                input: ['resources/js/app.ts'],
-                ssr: 'resources/js/ssr.ts',
-                refresh: true,
-            }),
-            tailwindcss(),
-            vue({
-                template: {
-                    transformAssetUrls: {
-                        base: null,
-                        includeAbsolute: false,
-                    },
-                },
-            }),
-            vueJsx(),
+      }),
+      vueJsx(),
+      Components({
+        deep: true,
+        resolvers: [NaiveUiResolver()],
+        collapseSamePrefixes: true,
+        dirs: ['resources/js/components'],
+        allowOverrides: true,
+      }),
+      AutoImport({
+        dts: './auto-imports.d.ts',
+        dirs: ['resources/js/utils/**', 'resources/js/composables/**'],
+        imports: [
+          {
+            'vue-router': ['useRoute', 'useRouter'],
+            vue: [
+              'h',
+              'nextTick',
+              'provide',
+              'inject',
+              'watch',
+              'watchEffect',
+              'ref',
+              'shallowRef',
+              'reactive',
+              'computed',
+              'useAttrs',
+              'onMounted',
+            ],
+            'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar'],
+          },
         ],
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, './resources/js'),
-            },
-        },
-    }
-});
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './resources/js'),
+      },
+    },
+  }
+})
