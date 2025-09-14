f7f867dca42590dd6956e8f131b3f45ce2135e09
Daniel Ogirev <marsik025@gmail.com>
2025-09-14 21:33:10 +0300

    chore: enabled model strict mode

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,11 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Models\User;
 use App\Modules\Post\Authorization\PostPolicy;
 use App\Modules\Post\Models\Post;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,5 +27,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::policy(Post::class, PostPolicy::class);
+
+        Model::shouldBeStrict();
+        Relation::enforceMorphMap([
+            'user' => User::class,
+        ]);
     }
 }
