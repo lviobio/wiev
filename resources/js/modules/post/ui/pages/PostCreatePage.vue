2023d2fca8619479594847182111ff0829d95aaf
Daniel Ogirev <marsik025@gmail.com>
2025-11-30 03:04:21 +0300

    feat(posts): show post after create

--- resources/js/modules/post/ui/pages/PostCreatePage.vue
+++ resources/js/modules/post/ui/pages/PostCreatePage.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
 import { usePostRepository } from '@/modules/post/repositories/PostRepository'
+import { postRouteGenerator } from '@/modules/post/router/names'
 import { Create } from '@/modules/post/ui/components'
 
+const router = useRouter()
 const repository = usePostRepository()
+
+function handleCreated(result: Awaited<ReturnType<typeof repository.create>>) {
+  router.push(postRouteGenerator.show(result.data.id))
+}
 </script>
 
 <template>
   <NFlex>
-    <Create.Component :repository="repository" />
+    <Create.Component :repository="repository" @created="handleCreated" />
   </NFlex>
 </template>
 
