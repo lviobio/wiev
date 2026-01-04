2269ff85555e6afe519c6fb4a0e813ab90c0755d
Daniel Ogirev <marsik025@gmail.com>
2026-01-04 21:35:55 +0300

    feat: createContext util

---
+++ resources/js/core/utils/context/index.ts
@@ -0,0 +1,52 @@
+import { injectLocal, provideLocal } from '@vueuse/core'
+import type { DefineComponent, PropType } from 'vue'
+import { defineComponent } from 'vue'
+
+type ProviderType<T> = DefineComponent<{
+  value: { type: PropType<T>; required: true }
+}>
+export type ReturnContextType<T> = {
+  readonly key: InjectionKey<T>
+  get: () => T
+  init: () => void
+  Provider: ProviderType<T>
+}
+
+export function createContext<T extends Record<string, unknown>>(
+  key: InjectionKey<T>,
+  defaultFactory: () => T,
+): ReturnContextType<T> {
+  const get = () => {
+    return injectLocal<T>(
+      key,
+      () => {
+        console.warn('[createContext] parent context not found, using default factory')
+
+        return defaultFactory()
+      },
+      true,
+    )
+  }
+
+  const init = () => {
+    const defaultValue = defaultFactory()
+
+    provideLocal(key, defaultValue)
+  }
+
+  const Provider: ProviderType<T> = defineComponent({
+    name: 'ContextProvider',
+    props: {
+      value: {
+        type: Object as PropType<T>,
+        required: true,
+      },
+    },
+    setup(props, { slots }) {
+      init()
+      return () => slots.default?.()
+    },
+  })
+
+  return { key, get, init, Provider }
+}
